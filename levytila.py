273ad48c9d3c3e5bf61416db7cbd71c5ce443360
Otto Puttonen <puttonenotto08@gmail.com>
2026-09-24 12:14:25 +0300

    Ensimmäinen versio

---
+++ levytila.py
@@ -0,0 +1,15 @@
+import shutil
+
+
+def tarkista_levytila():
+    try:
+        total, used, free = shutil.disk_usage("C:\\")
+
+        vapaa = free // (1024 ** 3)
+        kaytetty = used // (1024 ** 3)
+
+        print(f"Vapaata levytilaa: {vapaa} GiB")
+        print(f"Käytettyä levytilaa: {kaytetty} GiB")
+
+    except Exception as e:
+        print(f"Virhe levytilan tarkistuksessa: {e}")
